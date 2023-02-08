5075dcdadd911e2ecfd39f6f2cdcb5f3963b99b1
everly-gif <everlyprecia@gmail.com>
2023-02-08 09:44:50 +0530

    add homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { HomeComponent } from './home/home.component';
+import { LatestMoviesComponent } from './latest-movies/latest-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    NewsletterComponent,
+    HomeComponent,
+    LatestMoviesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/latest-movies/latest-movies.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LatestMoviesComponent } from './latest-movies.component';
+
+describe('LatestMoviesComponent', () => {
+  let component: LatestMoviesComponent;
+  let fixture: ComponentFixture<LatestMoviesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LatestMoviesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LatestMoviesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
